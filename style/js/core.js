049f4d4344d954edd2ea43bc15a98da9fa9137e9
Rodolphe <rodolphe@testapic.com>
2018-08-22 12:00:24 +0200

    instagram feed added on homepage

--- style/js/core.js
+++ style/js/core.js
@@ -1,6 +1,17 @@
 /**
  * Created by leguian on 20/08/2018.
  */
+MicroModal.init({
+    openTrigger: 'data-modal-open',
+    closeTrigger: 'data-modal-close',
+    disableScroll: true,
+    disableFocus: false,
+    awaitCloseAnimation: false,
+    debugMode: true
+});
+
+tippy('.tooltip');
+
 jQuery(document).ready(function ($) {
     try {
         $("#instagram").jqinstapics({
@@ -8,17 +19,6 @@ jQuery(document).ready(function ($) {
             "access_token": "3567011117.674061d.59160f04ba3c477b9ee08304569fa270",
             "count": 15
         });
-
-        MicroModal.init({
-            openTrigger: 'data-modal-open',
-            closeTrigger: 'data-modal-close',
-            disableScroll: true,
-            disableFocus: false,
-            awaitCloseAnimation: false,
-            debugMode: true
-        });
-
-        tippy('.tooltip');
 
     } catch (err) {
         console.log(err)
