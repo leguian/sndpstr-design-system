af2df12ce54be7cff6265468d6545280354bb329
Rodolphe <rodolphe@testapic.com>
2018-08-22 08:54:57 +0200

    Homepage

--- style/js/core.js
+++ style/js/core.js
@@ -1,7 +1,13 @@
 /**
  * Created by leguian on 20/08/2018.
  */
-
+var feed = new Instafeed({
+    get: 'popular',
+    tagName: 'awesome',
+    clientId: '1df79a5058474d36adfb3465f073b1a7',
+    template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>'
+});
+feed.run();
 MicroModal.init({
     openTrigger: 'data-modal-open',
     closeTrigger: 'data-modal-close',
